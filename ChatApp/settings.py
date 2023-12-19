ecbdc975ae72a66917b34d25c26050790e279662
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-12-19 17:02:39 -0300

    comecando a integrar com banco de dados

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,9 +79,13 @@ ASGI_APPLICATION = 'ChatApp.asgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+  'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chat',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost', 
+        'PORT': '3306', 
     }
 }
 
@@ -120,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- chat/consumers.py
+++ chat/consumers.py
@@ -9,11 +9,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
         await self.accept()
+
     async def disconnect(self , close_code):
         await self.channel_layer.group_discard(
             self.roomGroupName , 
             self.channel_name
         )
+
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
@@ -24,6 +26,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 "message" : message , 
                 "username" : username ,
             })
+        
     async def sendMessage(self , event) : 
         message = event["message"]
         username = event["username"]
